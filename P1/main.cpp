85694d9e036f8aa0a27311f8572f3ca8e1d31c47
Gonzalo <glopcas@krutt.org>
2022-10-19 18:42:11 +0200

    Práctica integral finalizada

--- P1/main.cpp
+++ P1/main.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
-int main() {
-    std::cout << "Proyecto nuevo para la práctica 1 de SCD" << std::endl;
-    return 0;
+void funcion_hebra_1( ) // función que va a ejecutar la hebra primera
+{ for( unsigned long i = 0 ; i < 5000 ; i++ )
+        cout << "hebra 1, i == " << i << endl ;
+}
+void funcion_hebra_2( ) // función que va a ejecutar la hebra segunda
+{ for( unsigned long i = 0 ; i < 5000 ; i++ )
+        cout << "hebra 2, i == " << i << endl ;
+}
+int main()
+{
+    thread hebra1( funcion_hebra_1 ), // crear hebra1 ejecutando funcion_hebra_1
+    hebra2( funcion_hebra_2 ); // crear hebra2 ejecutando funcion_hebra_2
+// ... finalizacion ....
+    hebra1.join();
+    hebra2.join();
 }
