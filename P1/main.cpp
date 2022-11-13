b471396b3df514bee8b81f687cffc1504a35106f
Gonzalo <glopcas@krutt.org>
2022-11-13 18:31:47 +0100

    Comienzo de la práctica 2 (monitores).

--- P1/main.cpp
+++ P1/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <thread>
+#include "test.h"
 
 using namespace std;
 
 int main() {
-    std::cout << "Proyecto nuevo para la práctica 1 de SCD" << std::endl;
-    return 0;
+
+    print();
 }
